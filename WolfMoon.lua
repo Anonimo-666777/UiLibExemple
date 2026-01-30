e918bc7d4f1dfc3bcb5e1bde14749565fcf2e813
Anonimo-666777 <davidgames3ddark@gmail.com>
2026-01-30 14:37:32 -0300

    Atualizar o WolfMoon.lua

--- WolfMoon.lua
+++ WolfMoon.lua
@@ -4,7 +4,7 @@ OrionLib:MakeNotification({
 	Name = "Welcome",
 	Content = "script by davidgames3d,menkato e outros",
 	Image = "rbxassetid://4483345998",
-	Time = 5
+	Time = 14
 })
 
 local Window = OrionLib:MakeWindow({Name = "Wolf Moon🐺🌜 HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
