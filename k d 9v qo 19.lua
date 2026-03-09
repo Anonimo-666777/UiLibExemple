9e27962c9bcdc2316d5b73bfa9d84f8ed6946068
Anonimo-666777 <davidgames3ddark@gmail.com>
2026-03-09 00:00:21 -0300

    Atualizar o k d 9v qo 19.lua

--- k d 9v qo 19.lua
+++ k d 9v qo 19.lua
@@ -1,1 +1,91 @@
--- ainda estou programando
+local Win = NexusUI:MakeWindow({
+    Title = "David Hub",
+    SubTitle = "v1.0 by davidgames3d", 
+    Theme = "Neon",
+    RGBBorder = true,
+    LogoId    = "rbxassetid://132152602986684", 
+}) 
+
+Win:SetSize(600) 
+Win:SetOpacity(0.4)
+
+-- tabs
+local Home = Win:MakeTab({
+    Name = "Home",
+    Icon = "rbxassetid://123456",
+})
+
+local Local = Win:MakeTab({
+    Name = "Player",
+    Icon = "rbxassetid://123456",
+})
+
+local Info = Win:MakeTab({
+    Name = "Info",
+    Icon = "rbxassetid://123456",
+})
+
+local Config = Win:MakeTab({
+    Name = "Config",
+    Icon = "rbxassetid://123456",
+})
+
+-- icon/image Label
+
+Home:MakeImage({
+    Image  = "rbxassetid://132152602986684",
+    Height = 150,
+    Desc   = "Logo do hub",
+})
+
+-- Sections home
+Home:MakeSection("Version")
+
+Home:MakeLabel("David Hub V1.0
+
+Home:MakeLabel("Lazarus/Lazarios UI Lib V1.0.0 Beta") 
+
+Home:MakeSection("Créditos") 
+
+Home:MakeImage({
+    Image  = "rbxassetid://5597737428",
+    Height = 90,
+    Desc   = "Youtube",
+})
+
+Home:MakeInput({
+	Name = "Youtube do criador",
+	Placeholder = "https://youtube.com/@davidgames3d?si=sIBVlOB3inq3sCHb",
+	Callback = function(text, enter)
+		print("Nome:", text)
+	end,
+})
+
+Home:MakeInput({
+	Name = "Discord do criador",
+	Placeholder = "https://discord.gg/nMGZGk5Tj",
+	Callback = function(text, enter)
+		print("Nome:", text)
+	end,
+})
+
+-- Section Info
+Info:MakeSection("Version") 
+
+Info:MakeLabel("Lazarus UI Lib v1.0.0 beta")
+
+Info:MakeLabel("David Hub V1.0") 
+
+Info:MakeSection("curiosidades e fatos") 
+
+Info:MakeLabel("Esse script usa uma Lib chamada Lazarus UI Lib ou lazarios feita por mim") 
+
+Info:MakeLabel("Eu faço scripts só pelo celular sei programar pouco mas tô aprendendo") 
+
+Info:MakeLabel("Eu fiz o site da key com o mimo gratuitamente sem saber scriptar css, js nem html") 
+
+-- Section LocalPlayer
+Local:MakeSection("scripts") 
+
+-- Section Configuração
+Config:MakeSection("Configurações") 
