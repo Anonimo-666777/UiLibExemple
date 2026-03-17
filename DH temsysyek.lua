8066a269448c021a73b50391bbb777dfba538f1d
Anonimo-666777 <davidgames3ddark@gmail.com>
2026-03-17 19:19:56 -0300

    Criar DH temsysyek.lua

---
+++ DH temsysyek.lua
@@ -0,0 +1,30 @@
+local NexusUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Anonimo-666777/Lazarios-ui-library/main/CoreLib.lua"))()
+
+NexusUI:MakeKeySystem({
+    Title    = "David Hub | KeySystem",
+    SubTitle = "Entre no site e complete as etapas para obter a Key!",
+    Site     = "https://i5j7hy.mimo.run/index.html",
+    Key      = "david2026",
+    Key2     = "menkato123",
+    Key3     = "nexusUi",
+    Key4     = "david",
+    Key5     = "01839174",
+    Key6     = "key1083",
+    Key7     = "ap103820qkdn",
+    Key8     = "menkato",
+    Key9     = "ooiii2",
+    Key10   = "Key10839183", 
+    Key11   = "Key07381639", 
+    Key12   = "10389173", 
+    Key13   = "qo1083kEy", 
+    Key14   = "Key1#10aj0183", 
+    Key15   = "KeY019A9vAM", 
+    Key16   = "KeyL0l1P0qknaoq", 
+    Key17  = "L1a0b2310an1038qkqon10", 
+    OnSuccess = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Anonimo-666777/UiLibExemple/main/k%20d%209v%20qo%2019.lua"))()
+    end,
+    OnFail = function()
+        print("Key incorreta, saindo...")
+    end,
+})
